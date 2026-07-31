52937cdf2a7ee0e54d4d841f6f66b8935bfa7e02
Julien Huang <julien.h.dev@gmail.com>
2026-07-31 10:30:31 +0200

    trim unused exports

--- code/core/src/component-meta/index.ts
+++ code/core/src/component-meta/index.ts
@@ -1,11 +1,4 @@
 export { ComponentMetaManager, isFileInDir, sortTSConfigs } from './ComponentMetaManager.ts';
 export { parseTsconfigCommandLine } from './parse-tsconfig.ts';
 export type { FileExtensionInfo, TsconfigParserModule } from './parse-tsconfig.ts';
-export type {
-  ComponentMetaFileSystem,
-  ComponentMetaProjectBase,
-  ComponentMetaProjectFactory,
-  FileChange,
-  FileChangeType,
-  ProjectCommandLine,
-} from './types.ts';
+export type { ComponentMetaProjectBase, ComponentMetaProjectFactory, FileChange } from './types.ts';
