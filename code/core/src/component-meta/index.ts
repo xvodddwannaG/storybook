0b6fb8e85c69378835f354afe464f0abccb0372f
Julien Huang <julien.h.dev@gmail.com>
2026-07-31 10:23:35 +0200

    refactor: move ak parseTsconfigCommandLine to core

--- code/core/src/component-meta/index.ts
+++ code/core/src/component-meta/index.ts
@@ -1,4 +1,6 @@
 export { ComponentMetaManager, isFileInDir, sortTSConfigs } from './ComponentMetaManager.ts';
+export { parseTsconfigCommandLine } from './parse-tsconfig.ts';
+export type { FileExtensionInfo, TsconfigParserModule } from './parse-tsconfig.ts';
 export type {
   ComponentMetaFileSystem,
   ComponentMetaProjectBase,
